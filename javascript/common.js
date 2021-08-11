224759b0e5cb8c5d646e2e2998f6534cef64a269
Daniella Vasconcellos <danivasco@msn.com>
2021-08-11 14:22:34 -0300

    aa

---
+++ javascript/common.js
@@ -0,0 +1,16 @@
+const redButton = document.getElementById('savechang');
+const namee = document.getElementById('name');
+const lastName = document.getElementById('last-name');
+const passport = document.getElementById('passport');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+
+
+
+function checkInputs(){
+    namee.value;
+    lastName.value;
+    passport.value;
+    email.value;
+    phone.value;
+}
